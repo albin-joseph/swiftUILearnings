ae1f21af359ca2f9d92c56ef271efd61d6c50796
albin-joseph <albinjoseph007@gmail.com>
2021-05-29 08:55:00 +0530

    berries project initial commit

---
+++ berries/berries/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  berries
+//
+//  Created by Albin Joseph on 29/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ berries/berries/berriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  berriesApp.swift
+//  berries
+//
+//  Created by Albin Joseph on 29/05/21.
+//
+
+import SwiftUI
+
+@main
+struct berriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
